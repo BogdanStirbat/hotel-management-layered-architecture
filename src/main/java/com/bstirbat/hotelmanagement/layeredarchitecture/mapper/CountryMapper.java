816c21f266e1a2ee8a67eabb3b94157207cb03ba
Bogdan Stirbat <bogdan.stirbat@gmail.com>
2023-09-01 12:20:49 +0300

    fix the unmapped target warning

--- src/main/java/com/bstirbat/hotelmanagement/layeredarchitecture/mapper/CountryMapper.java
+++ src/main/java/com/bstirbat/hotelmanagement/layeredarchitecture/mapper/CountryMapper.java
@@ -4,6 +4,7 @@ import com.bstirbat.hotelmanagement.layeredarchitecture.model.dto.request.Countr
 import com.bstirbat.hotelmanagement.layeredarchitecture.model.dto.response.CountryDto;
 import com.bstirbat.hotelmanagement.layeredarchitecture.model.entity.Country;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -11,6 +12,7 @@ public interface CountryMapper {
 
   CountryMapper INSTANCE = Mappers.getMapper(CountryMapper.class);
 
+  @Mapping(target = "id", ignore = true)
   Country toEntity(CountryCreateDto dto);
 
   CountryDto toDto(Country entity);
