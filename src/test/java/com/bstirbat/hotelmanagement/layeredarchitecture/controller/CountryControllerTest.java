171f112bf5b7119f32152e4cb305390a9745ce6d
Bogdan Stirbat <bogdan.stirbat@gmail.com>
2023-09-05 16:47:26 +0300

    use Mockito for the controller unit tests

--- src/test/java/com/bstirbat/hotelmanagement/layeredarchitecture/controller/CountryControllerTest.java
+++ src/test/java/com/bstirbat/hotelmanagement/layeredarchitecture/controller/CountryControllerTest.java
@@ -1,66 +1,36 @@
 package com.bstirbat.hotelmanagement.layeredarchitecture.controller;
 
-import static com.bstirbat.hotelmanagement.layeredarchitecture.constants.Paths.COUNTRIES;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
 
-import com.bstirbat.hotelmanagement.layeredarchitecture.config.JwtAuthenticationFilter;
-import com.bstirbat.hotelmanagement.layeredarchitecture.config.SecurityConfiguration;
-import com.bstirbat.hotelmanagement.layeredarchitecture.config.user.UserDetailsServiceImpl;
-import com.bstirbat.hotelmanagement.layeredarchitecture.enums.Role;
 import com.bstirbat.hotelmanagement.layeredarchitecture.model.dto.request.CountryCreateDto;
+import com.bstirbat.hotelmanagement.layeredarchitecture.model.dto.response.CountryDto;
 import com.bstirbat.hotelmanagement.layeredarchitecture.model.entity.Country;
-import com.bstirbat.hotelmanagement.layeredarchitecture.model.entity.User;
-import com.bstirbat.hotelmanagement.layeredarchitecture.repository.UserRepository;
 import com.bstirbat.hotelmanagement.layeredarchitecture.service.CountryService;
-import com.bstirbat.hotelmanagement.layeredarchitecture.service.JwtService;
-import com.bstirbat.hotelmanagement.layeredarchitecture.service.impl.UserServiceImpl;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
 
-@Import({JwtAuthenticationFilter.class, UserDetailsServiceImpl.class, UserServiceImpl.class, SecurityConfiguration.class})
-@WebMvcTest(CountryController.class)
+
 class CountryControllerTest {
 
-  @Autowired
-  private MockMvc mockMvc;
-
-  @MockBean
   private CountryService countryService;
-
-  @MockBean
-  private UserRepository userRepository;
-
-  @MockBean
-  private JwtService jwtService;
-
-  private final ObjectMapper objectMapper = new ObjectMapper();
+  private CountryController countryController;
 
   @BeforeEach
   public void setUp() {
-    User user = new User();
-    user.setRole(Role.ADMIN);
-    user.setEmail("admin@test.com");
-    user.setPassword("secret");
-
-    when(userRepository.findByEmail(any())).thenReturn(Optional.of(user));
-    when(jwtService.extractUserName(any())).thenReturn("admin@test.com");
-    when(jwtService.isTokenValid(any(), any())).thenReturn(true);
+    countryService = mock(CountryService.class);
+    countryController = new CountryController(countryService);
   }
 
   @Test
-  void createCountry() throws Exception {
+  void createCountry() {
     CountryCreateDto createDto = new CountryCreateDto();
     createDto.setName("Germany");
     createDto.setCountryCode("DE");
@@ -72,14 +42,40 @@ class CountryControllerTest {
 
     when(countryService.create(any())).thenReturn(country);
 
-    this.mockMvc
-        .perform(
-            post(COUNTRIES)
-                .header("Authorization", "Bearer test")
-                .accept(MediaType.APPLICATION_JSON)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(createDto)))
-        .andDo(print())
-        .andExpect(status().isCreated());
+    ResponseEntity<CountryDto> responseEntity = countryController.create(createDto);
+    assertEquals(CREATED, responseEntity.getStatusCode());
+
+    CountryDto responseDto = responseEntity.getBody();
+    assertNotNull(responseDto);
+    assertEquals(country.getId(), responseDto.getId());
+    assertEquals(country.getName(), responseDto.getName());
+    assertEquals(country.getCountryCode(), responseDto.getCountryCode());
+  }
+
+  @Test
+  void getById() {
+    Country country = new Country();
+    country.setId(1L);
+    country.setName("Germany");
+    country.setCountryCode("DE");
+
+    when(countryService.getById(1L)).thenReturn(country);
+
+    ResponseEntity<CountryDto> responseEntity = countryController.getById(1L);
+    assertEquals(OK, responseEntity.getStatusCode());
+
+    CountryDto responseDto = responseEntity.getBody();
+    assertNotNull(responseDto);
+    assertEquals(country.getId(), responseDto.getId());
+    assertEquals(country.getName(), responseDto.getName());
+    assertEquals(country.getCountryCode(), responseDto.getCountryCode());
+  }
+
+  @Test
+  void findAll() {
+    when(countryService.findAll(any())).thenReturn(Page.empty());
+
+    ResponseEntity<Page<CountryDto>> responseEntity = countryController.findAll(0, 20);
+    assertEquals(OK, responseEntity.getStatusCode());
   }
 }
